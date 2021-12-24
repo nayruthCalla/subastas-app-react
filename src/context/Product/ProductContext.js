0e2e3a6762fc0d1c61907dd0c153f3309af9fe7a
nayruthCalla <nayruth.calla@gmail.com>
2021-12-23 22:36:29 -0500

    warning netlify

--- src/context/Product/ProductContext.js
+++ src/context/Product/ProductContext.js
@@ -17,7 +17,7 @@ export const ProductProvider = ({ children }) => {
     try {
       const res = await axios.get("products");
       setProductContex(res.data);
-      console.log(res.data);
+      // console.log(res.data);
     } catch (e) {
       // console.log(e)
     }
