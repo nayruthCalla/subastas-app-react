737056c9391d42304bd0cdb93d3172e4dfb90a4a
nayruthCalla <nayruth.calla@gmail.com>
2021-12-24 01:26:33 -0500

    responsive page not found

--- src/conponents/Pages/NotFound/NotFound.jsx
+++ src/conponents/Pages/NotFound/NotFound.jsx
@@ -3,9 +3,11 @@ import styles from '../../../css/styles.module.css'
 
 const NotFound = () => {
   return (
-    <div>
-      <h2 className={styles.notFoundNum} >404</h2>
-      <h2 className={styles.notFoundText} >HUBO UN ERROR</h2>
+    <div className={styles.notFoundCont}>
+      <div className={styles.notTitle}>
+        <h2 className={styles.notFoundNum} >404</h2>
+        <h2 className={styles.notFoundText} >HUBO UN ERROR</h2>
+      </div>
       <img className={styles.notFoundImg} src = {image} alt=""/>      
     </div>
   )
